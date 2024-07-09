1049b7e658207226acd894217aa9761f8622b6b8
Oyizaadewuyi <oyizaadewuyi@gmail.com>
2024-07-09 20:42:33 +0100

    timbu

--- src/pages/TimbuAddToCart/TimbuAddToCart.jsx
+++ src/pages/TimbuAddToCart/TimbuAddToCart.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from '../../components/Header/Header'
+import Header from '../../components/Header/Navbar'
 
 export const TimbuAddToCart = () => {
   return (
